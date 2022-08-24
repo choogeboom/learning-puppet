d6cede7a99b9800d67be616cd185d05949b9162b
Chris Hoogeboom <choogeboom@wayfair.com>
2022-08-24 16:14:23 -0400

    Update for section 4-5

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,21 @@
 node default {
+  file { '/root/README':
+    ensure => file,
+    content => "This is a readme\n",
+    owner   => 'root',
+  }
 }
 node 'puppet' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn}\n",
+    owner => 'root',
+  }
+}
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
 }
